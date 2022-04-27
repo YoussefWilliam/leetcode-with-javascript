96e5e930f85a774f7c2d3ffa0fece170936abaa8
YoussefWilliam <youssef.william997@gmail.com>
2022-04-27 11:05:15 +0200

    Submit: LC125_isPalindrom

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,26 @@
-function isPalindrome(s) {}
+/* 
+1. Sanitize the input string and remove all the non alphanumeric characters.
+2. Create a left and right pointers, initially at the start till the end of the string.
+3. While left is less than right:
+    If left does not match right => return false
+    else => return true
+
+You end up with:
+ - Time complexity is O(n) => since you iterated each character in the string.
+ - Space Complexity is O(1) => because left and right pointers take up constant space.
+*/
+
+function isPalindrome(s) {
+  s = s.toLowerCase().replace(/[\W_]/g, "");
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
 
 module.exports = isPalindrome;
