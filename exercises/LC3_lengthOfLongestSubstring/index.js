5e6f8571e5af50bab9eb53dc07bfabd3dc38b260
YoussefWilliam <youssef.william997@gmail.com>
2022-10-22 14:57:31 +0200

    [add] Submit LC_5 longestSubstring

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,23 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+function lengthOfLongestSubstring(s) {
+  let leftPointer = 0;
+  let rightPointer = 0;
+  let max = 0;
+
+  let hashSet = new Set();
+  while (rightPointer < s.length) {
+    if (!hashSet.has(s[rightPointer])) {
+      hashSet.add(s[rightPointer]);
+      rightPointer++;
+      max = Math.max(hashSet.size, max);
+    } else {
+      hashSet.delete(s[leftPointer]);
+      leftPointer++;
+    }
+  }
+  return max;
+}
 
 module.exports = lengthOfLongestSubstring;
