4703e6afc1b1ddb4565b3b797c38feb96f8c5286
YoussefWilliam <youssef.william997@gmail.com>
2022-10-22 15:14:00 +0200

    [solve] Submit LC_1 two sumbs

--- exercises/LC1_TwoSum/index.js
+++ exercises/LC1_TwoSum/index.js
@@ -6,6 +6,13 @@
 // twoSum([2,7,11,15], 9) --> [0,1]
 // twoSum([3,5,4], 9) --> [1,2]
 
-function twoSum(arr, target) {}
+function twoSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    let sub = target - arr[i];
+    if (arr.includes(sub) && arr.indexOf(sub) !== i) {
+      return [i, arr.indexOf(sub)];
+    }
+  }
+}
 
 module.exports = twoSum;
