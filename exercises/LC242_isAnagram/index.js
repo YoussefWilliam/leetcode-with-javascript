7a8d02b083fefcd3ba1c24e1c1560a50ad62a640
YoussefWilliam <youssef.william997@gmail.com>
2022-10-24 10:21:24 +0200

    [add] Submit valid anagram

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -1,3 +1,20 @@
-function isAnagram(s, t) {}
+function isAnagram(s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+  const s_hashmap = s
+    .split("")
+    .reduce((map, key) => map.set(key, (map.get(key) || 0) + 1), new Map());
+
+  for (let i = 0; i < t.length; i++) {
+    const tChar = t[i];
+    if (!s_hashmap.get(tChar)) {
+      return false;
+    } else {
+      s_hashmap.set(tChar, s_hashmap.get(tChar) - 1);
+    }
+  }
+  return true;
+}
 
 module.exports = isAnagram;
--- exercises/LC2_addTwoNumbers/index.js
+++ exercises/LC2_addTwoNumbers/index.js
@@ -13,6 +13,25 @@ class ListNode {
   }
 }
 
-function addTwoNumbers(l1, l2) {}
+function spreadList(list) {
+  let result = "";
+  while (list) {
+    result += list.val;
+    list = list.next;
+  }
+  return result.split("").reverse().join("");
+}
+function addTwoNumbers(l1, l2) {
+  let list1 = spreadList(l1);
+  let list2 = spreadList(l2);
+  let sumNodes = parseInt(list1) + parseInt(list2);
+  let reversedSumNodesArray = String(sumNodes).split("");
+  let resultNode = null;
+  reversedSumNodesArray.map((digit) => {
+    const head = new ListNode(Number(digit), resultNode);
+    resultNode = head;
+  });
+  return resultNode;
+}
 
 module.exports = addTwoNumbers;
