1ba42fb32b3e30465e125d14cad2518add81e102
YoussefWilliam <youssef.william997@gmail.com>
2022-10-24 10:36:07 +0200

    [add] Submit group anagrams

--- exercises/LC49_groupAnagrams/index.js
+++ exercises/LC49_groupAnagrams/index.js
@@ -1,3 +1,15 @@
-function groupAnagrams(strs) {}
+function groupAnagrams(strs) {
+  let grouped = {};
+
+  for (let i = 0; i < strs.length; i++) {
+    let word = strs[i];
+    let key = word.split("").sort().join("");
+    if (!grouped[key]) {
+      grouped[key] = [];
+    }
+    grouped[key].push(word);
+  }
+  return Object.values(grouped);
+}
 
 module.exports = groupAnagrams;
