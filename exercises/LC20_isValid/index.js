32341fa0607b92c635e99fa81aa500798d9fd552
YoussefWilliam <youssef.william997@gmail.com>
2022-10-27 08:55:07 +0200

    Submit isValid

--- exercises/LC20_isValid/index.js
+++ exercises/LC20_isValid/index.js
@@ -1,3 +1,15 @@
-const isValid = s => {};
+const isValid = (s) => {
+  let stack = [];
+  let pairMap = { "{": "}", "[": "]", "(": ")" };
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (pairMap[char]) {
+      stack.push(char);
+    } else if (pairMap[stack.pop()] !== char) {
+      return false;
+    }
+  }
+  return stack.length === 0;
+};
 
 module.exports = isValid;
