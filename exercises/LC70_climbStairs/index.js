6e72c2ee672f057987130220174be1bd67c51b13
YoussefWilliam <youssef.william997@gmail.com>
2022-10-27 09:14:05 +0200

    Submit climbStairs

--- exercises/LC70_climbStairs/index.js
+++ exercises/LC70_climbStairs/index.js
@@ -1,3 +1,27 @@
-const climbStairs = n => {};
+/**
+ *
+ * @param {*} n
+ * @param {*} memo
+ * @returns a cached result
+ */
+const climbStair1 = (n, memo = {}) => {
+  if (memo[n]) return memo[n];
+  if (n <= 3) return n;
+  memo[n] = climbStair1(n - 1, memo) + climbStair1(n - 2, memo);
+  return memo[n];
+};
 
-module.exports = climbStairs;
+/**
+ * Normal flow by having an array of cache implemented within the function
+ */
+
+const climbStair2 = (n) => {
+  if (n <= 3) return n;
+  let ways = [0, 1, 2, 3];
+  for (let i = 4; i <= n; i++) {
+    ways.push(ways[i - 1] + ways[i - 2]);
+  }
+  return ways.pop();
+};
+
+module.exports = { climbStair1, climbStair2 };
--- exercises/LC70_climbStairs/test.js
+++ exercises/LC70_climbStairs/test.js
@@ -1,17 +1,37 @@
-const climbStairs = require("./index");
+const { climbStair1, climbStair2 } = require("./index");
 
-test("Returns 1 way to climb 1 stair", () => {
-  expect(climbStairs(1)).toEqual(1);
+describe("Testing first function using external cache", () => {
+  test("Returns 1 way to climb 1 stair", () => {
+    expect(climbStair1(1)).toEqual(1);
+  });
+
+  test("Returns 2 ways to climb 2 stairs", () => {
+    expect(climbStair1(2)).toEqual(2);
+  });
+
+  test("Returns 3 ways to climb 3 stairs", () => {
+    expect(climbStair1(3)).toEqual(3);
+  });
+
+  test("Returns 5 ways to climb 4 stairs", () => {
+    expect(climbStair1(4)).toEqual(5);
+  });
 });
 
-test("Returns 2 ways to climb 2 stairs", () => {
-  expect(climbStairs(2)).toEqual(2);
-});
+describe("Testing second function using predefined array", () => {
+  test("Returns 1 way to climb 1 stair", () => {
+    expect(climbStair2(1)).toEqual(1);
+  });
 
-test("Returns 3 ways to climb 3 stairs", () => {
-  expect(climbStairs(3)).toEqual(3);
-});
+  test("Returns 2 ways to climb 2 stairs", () => {
+    expect(climbStair2(2)).toEqual(2);
+  });
 
-test("Returns 5 ways to climb 4 stairs", () => {
-  expect(climbStairs(4)).toEqual(5);
+  test("Returns 3 ways to climb 3 stairs", () => {
+    expect(climbStair2(3)).toEqual(3);
+  });
+
+  test("Returns 5 ways to climb 4 stairs", () => {
+    expect(climbStair2(4)).toEqual(5);
+  });
 });
