36f0a4abd87f89ad5f8b487990181b50263848bd
YoussefWilliam <youssef.william997@gmail.com>
2022-11-04 14:43:20 +0200

    Submit longest subsequence

--- exercises/LC300_lengthOfLIS/index.js
+++ exercises/LC300_lengthOfLIS/index.js
@@ -1,3 +1,15 @@
-function lengthOfLIS(nums) {}
+function lengthOfLIS(nums) {
+  if (nums.length === 0) return 0;
+  let cache = new Array(nums.length).fill(1);
+  for (let j = 1; j < nums.length; j++) {
+    for (let i = 0; i < j; i++) {
+      if (nums[j] > nums[i]) {
+        cache[j] = Math.max(cache[i] + 1, cache[j]);
+      }
+    }
+  }
+  cache.sort((a, b) => a - b);
+  return cache[nums.length - 1];
+}
 
 module.exports = lengthOfLIS;
