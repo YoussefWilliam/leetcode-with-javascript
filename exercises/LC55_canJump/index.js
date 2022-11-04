a0fabb32d131806b7ab0e6de7d70d11830151817
YoussefWilliam <youssef.william997@gmail.com>
2022-11-04 13:02:14 +0200

    Submit LC_55

--- exercises/LC55_canJump/index.js
+++ exercises/LC55_canJump/index.js
@@ -1,3 +1,16 @@
-function canJump(nums) {}
+function canJump(nums) {
+  const arrayLength = nums.length;
+  let cache = new Array(arrayLength).fill(false);
+  cache[0] = true;
+  for (let j = 1; j < arrayLength; j++) {
+    for (let i = 0; i < j; i++) {
+      if (cache[i] && i + nums[i] >= j) {
+        cache[j] = true;
+        break;
+      }
+    }
+  }
+  return cache[arrayLength - 1];
+}
 
 module.exports = canJump;
