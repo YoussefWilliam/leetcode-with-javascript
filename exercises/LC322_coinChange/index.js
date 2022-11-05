40478dc23a6de75e9ce2d767c4ac071b61a97d78
YoussefWilliam <youssef.william997@gmail.com>
2022-11-05 10:31:18 +0200

    submit coin change

--- exercises/LC322_coinChange/index.js
+++ exercises/LC322_coinChange/index.js
@@ -1,3 +1,18 @@
-function coinChange(coins, amount) {}
+function coinChange(coins, amount) {
+  if (coins.length <= 1 && coins[0] != amount) return -1;
+  let cache = new Array(coins.length).fill(0);
+  coins.sort((a, b) => a - b);
+  let num = amount;
+  let i = coins.length - 1;
+  while (i >= 0 && num >= 0) {
+    if (num - coins[i] >= 0) {
+      num = num - coins[i];
+      cache[i] = cache[i] + 1;
+    } else {
+      i--;
+    }
+  }
+  return cache.reduce((acc, elem) => acc + elem);
+}
 
 module.exports = coinChange;
