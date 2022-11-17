2d5213f3738d38372607376f8afc8c2a443fcfac
YoussefWilliam <youssef.william997@gmail.com>
2022-11-17 13:55:19 +0200

    submit max area

--- exercises/LC11_maxArea/index.js
+++ exercises/LC11_maxArea/index.js
@@ -1,3 +1,20 @@
-function maxArea(height) {}
+function maxArea(height) {
+  let left = 0;
+  let right = height.length - 1;
+  let maxAreaBetweenTwoHeights = 0;
+
+  while (left < right) {
+    maxAreaBetweenTwoHeights = Math.max(
+      maxAreaBetweenTwoHeights,
+      Math.min(height[right], height[left]) * (right - left)
+    );
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return maxAreaBetweenTwoHeights;
+}
 
 module.exports = maxArea;
